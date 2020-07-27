382e44a4b08bb49af2eb6ae0322bfcef7840b443
proforty7 <proforty7@gmail.com>
2020-07-27 18:16:27 +0530

    Updates

---
+++ precache-manifest.abbab96e34872835d9e2cf18f1fa70e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4e9d844bbe62905983c9df43b84603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd81cdfbe95625626b49",
+    "url": "/static/css/main.89c04201.chunk.css"
+  },
+  {
+    "revision": "bc21cd5a66e4ec694fa1",
+    "url": "/static/js/2.ee00c408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee00c408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd81cdfbe95625626b49",
+    "url": "/static/js/main.67d8c08f.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "ebab8bd62f52f09501cd5883c94a82f8",
+    "url": "/static/media/hero-bg.ebab8bd6.jpg"
+  }
+]);
